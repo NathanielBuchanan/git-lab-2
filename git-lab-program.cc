8f38aeff1b17c3b47d3c03d161ca1aee704221c5
Nathaniel Buchanan <nb333218@ohio.edu>
2020-01-21 19:20:17 -0500

    Added git-lab-program.cc to GitHub

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nathaniel Buchanan>
+ *        Date: <January 21, 2020>
+ * Description: This is a C++ Program that will be on GitHub.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+
+}// main
